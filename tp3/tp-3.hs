7379c22145598f9cada6e36b92a53dadd2e00dfe
JulianVillarroel <julian.villarroel@alu.unq.edu.ar>
2023-09-05 22:40:01 -0300

    mas ejercicios

--- tp3/tp-3.hs
+++ tp3/tp-3.hs
@@ -50,4 +50,76 @@ ponerN :: Int -> Color -> Celda -> Celda
 ponerN 0 _ c = c
 ponerN n c ce = ponerN (n - 1) c (poner c ce)
 
+-- 1.2) Camino hacia el tesoro
 
+data Objeto = Cacharro | Tesoro                          deriving Show
+data Camino = Fin | Cofre [Objeto] Camino | Nada Camino  deriving Show
+
+-- Indica si hay un cofre con un tesoro en el camino.
+hayTesoro :: Camino -> Bool
+hayTesoro Fin = False
+hayTesoro (Cofre obs c) = contieneTesoro obs || hayTesoro c 
+hayTesoro (Nada c) = hayTesoro c
+
+contieneTesoro :: [Objeto] -> Bool
+contieneTesoro []       = False
+contieneTesoro (t : _)  = True 
+contieneTesoro (_ : os) = contieneTesoro os
+
+
+caminoConTesoro         = Cofre [Cacharro, Tesoro] (Nada (Cofre [Tesoro, Cacharro] Fin))
+caminoConTesoroMuyLejos = Nada (Nada (Nada (Nada (Nada (Nada (Nada (Nada (Nada (Nada (Cofre [Cacharro] Fin)))))))))) -- 10 pasos hasta el cofre
+caminoSinTesoro         = Nada (Fin)
+
+
+{-Indica la cantidad de pasos que hay que recorrer hasta llegar al primer cofre con un tesoro.
+Si un cofre con un tesoro está al principio del camino, la cantidad de pasos a recorrer es 0.
+Precondición: tiene que haber al menos un tesoro.
+-}
+
+pasosHastaTesoro :: Camino -> Int
+pasosHastaTesoro Fin = error "No hay tesoro"
+pasosHastaTesoro (Cofre _ _) = 0
+pasosHastaTesoro (Nada resto) = 1 + pasosHastaTesoro resto
+
+
+-- Indica si hay un tesoro en una cierta cantidad exacta de pasos. Por ejemplo, si el número de
+-- pasos es 5, indica si hay un tesoro en 5 pasos.
+hayTesoroEn :: Int -> Camino -> Bool
+hayTesoroEn n camino = hayTesoroEnAux n camino 0
+
+hayTesoroEnAux :: Int -> Camino -> Int -> Bool
+hayTesoroEnAux 0 (Cofre obs _) _ = contieneTesoro obs
+hayTesoroEnAux n (Cofre _ c) pasos = hayTesoroEnAux (n - 1) c (pasos + 1)
+hayTesoroEnAux n (Nada c) pasos = hayTesoroEnAux n c (pasos + 1)
+hayTesoroEnAux _ Fin _ = False
+
+-- Indica si hay al menos n tesoros en el camino.
+alMenosNTesoros :: Int -> Camino -> Bool
+alMenosNTesoros n camino = alMenosNTesorosEnElCamino n camino
+
+alMenosNTesorosEnElCamino :: Int -> Camino -> Bool
+alMenosNTesorosEnElCamino 0 _ = True
+alMenosNTesorosEnElCamino n Fin = False
+alMenosNTesorosEnElCamino n (Cofre obs c) =
+  alMenosNTesorosEnElCamino (n - contarTesorosEnLista obs) c
+alMenosNTesorosEnElCamino n (Nada c) = alMenosNTesorosEnElCamino n c
+
+contarTesorosEnLista :: [Objeto] -> Int
+contarTesorosEnLista [] = 0
+contarTesorosEnLista (Tesoro : obs) = 1 + contarTesorosEnLista obs
+contarTesorosEnLista (_ : obs) = contarTesorosEnLista obs
+
+-- 2. Tipos arbóreos
+
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)  deriving Show
+
+-- 1. Dado un árbol binario de enteros devuelve la suma entre sus elementos.
+sumarT :: Tree Int -> Int
+sumarT EmptyT          = 0
+sumarT (NodeT a ai ad) = a + sumarT ai + sumarT ad
+
+-- 2. Dado un árbol binario devuelve su cantidad de elementos, es decir, el tamaño del árbol (size en inglés).
+sizeT :: Tree a -> Int
+sizeT EmptyT = 0
+sizeT (NodeT _ iz de) = 1 + sizeT iz + sizeT de
